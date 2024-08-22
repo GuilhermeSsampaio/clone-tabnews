e13649ab4fb11751319a765fefaf998a576bb9a1
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-08-22 14:44:04 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home(){
-    return  <h1>Hello world</h1>
+function Home() {
+  return <h1>Hello world</h1>;
 }
 
 export default Home;
 //default porque é partir desse componente que um chama o outro
+function teste() {
+  console("erro");
+}
